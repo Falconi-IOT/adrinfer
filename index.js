561cbd4ac9528b02d61f339131444768e7c0ec8c
Falconi-IOT <marcosfalconi64@gmail.com>
2024-04-17 14:20:04 -0300

    Atualização 17 04

--- index.js
+++ index.js
@@ -17,65 +17,65 @@ const app = express();
 app.use(express.json());
 
 const allowCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
+    res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS, PATCH"
-  );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, PUT, POST, DELETE, OPTIONS, PATCH"
+    );
 
-  res.header("Access-Control-Allow-Credentials", "false");
+    res.header("Access-Control-Allow-Credentials", "false");
 
-  next();
+    next();
 };
 
-const verficaCHG = async function (emp) {
-  const produtos = [{ codigo: "0212234" }];
+const verficaCHG = async function(emp) {
+    const produtos = [{ codigo: "0212234" }];
 
-  for (const [index, dado] of produtos.entries()) {
-    const produto = await chgSrv.getProdutoByCodigo(emp, dado.codigo);
-    console.log(index, produto.codigo, produto.nome, produto.estoque);
-  }
-  return;
-};
-
-const refreshToken = async function (emp) {
-  const validade = shared.ValidarToken(emp);
-  console.log("validade: ", validade);
-  if (validade.minutos_restantes <= 60) {
-    emp = await bling.getAtualizaToken(emp);
-  }
-  return;
-};
-
-const iniciar = async function () {
-  let emp = {};
-
-  try {
-    emp = await empresaSrv.getEmpresa(1);
-  } catch (error) {
-    throw error;
-  }
-  try {
-    await verficaCHG(emp);
-    await refreshToken(emp);
-  } catch (error) {
-    if (error.response) {
-      console.log(error.response.data);
-    } else {
-      if (error.name == "MyExceptionDB") {
-        console.log(error);
-      } else {
-        res = { erro: "BAK-END", tabela: "Empresa", message: error.message };
-        console.log(res);
-      }
+    for (const [index, dado] of produtos.entries()) {
+        const produto = await chgSrv.getProdutoByCodigo(emp, dado.codigo);
+        console.log(index, produto.codigo, produto.nome, produto.estoque);
     }
-  }
-  empresaSrv.AtivarEmpresas();
+    return;
+};
+
+const refreshToken = async function(emp) {
+    const validade = shared.ValidarToken(emp);
+    console.log("validade: ", validade);
+    if (validade.minutos_restantes <= 60) {
+        emp = await bling.getAtualizaToken(emp);
+    }
+    return;
+};
+
+const iniciar = async function() {
+    let emp = {};
+
+    try {
+        emp = await empresaSrv.getEmpresa(1);
+    } catch (error) {
+        throw error;
+    }
+    try {
+        await verficaCHG(emp);
+        await refreshToken(emp);
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+        } else {
+            if (error.name == "MyExceptionDB") {
+                console.log(error);
+            } else {
+                res = { erro: "BAK-END", tabela: "Empresa", message: error.message };
+                console.log(res);
+            }
+        }
+    }
+    //empresaSrv.AtivarEmpresas();
 };
 
 app.use(allowCors);
@@ -92,7 +92,7 @@ app.use(express.static(path.join(__dirname, "/application")));
 
 //correção da rota do angular
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/application/index.html"));
+    res.sendFile(path.join(__dirname, "/application/index.html"));
 });
 
 iniciar();
@@ -100,5 +100,5 @@ iniciar();
 //console.log("AMBIENTE:", process.env.ENVIRONMENT);
 
 app.listen(PORT, () => {
-  console.log(`Servidor No Ar. Porta ${PORT}`);
+    console.log(`Servidor No Ar. Porta ${PORT}`);
 });
